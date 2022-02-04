44bb458aaf3eed6d3883ed8355d461f6a14d0e14
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2022-02-04 14:13:41 -0300

    Update superballastflora.lua

--- Lua/randomevents/superballastflora.lua
+++ Lua/randomevents/superballastflora.lua
@@ -23,12 +23,6 @@ event.Think = function ()
             local pump = pumps[Random.Range(1, #pumps + 1)]
             
             pump.InfectBallast("ballastflora", true)
-
-            if pump.item.CurrentHull then
-                pump.item.CurrentHull.BallastFlora.MaxAnger = 1000
-                pump.item.CurrentHull.BallastFlora.Anger = 1000
-                pump.item.CurrentHull.BallastFlora.PulseInflateSpeed = 100
-            end
         end
 
         ballastFloraTimer = Timer.GetTime() + event.Config.BallastFloraDelay
