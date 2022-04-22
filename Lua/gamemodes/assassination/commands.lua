75652b230c9ac37490941e84c4cd898e758de145
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2022-04-22 13:29:27 -0300

    fix traitor alive command

--- Lua/gamemodes/assassination/commands.lua
+++ Lua/gamemodes/assassination/commands.lua
@@ -3,9 +3,11 @@ local assassination = Traitormod.SelectedGamemode
 Traitormod.AddCommand("!traitoralive", function (client, args)
     for character, traitor in pairs(assassination.Traitors) do
         if not character.IsDead then
-            return Traitormod.Language.TraitorsAlive
+            Traitormod.SendMessage(client, Traitormod.Language.TraitorsAlive)
+            return true
         end
     end
 
-    return Traitormod.Language.AllTraitorsDead
+    Traitormod.SendMessage(client, Traitormod.Language.AllTraitorsDead)
+    return true
 end)
