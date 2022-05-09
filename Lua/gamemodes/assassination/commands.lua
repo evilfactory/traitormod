4b8b65067567bbed9629b73831268e2491586bcf
MassCraxx <masscraxx@users.noreply.github.com>
2022-05-09 03:11:36 +0200

    reenable !traitoralive for admins

--- Lua/gamemodes/assassination/commands.lua
+++ Lua/gamemodes/assassination/commands.lua
@@ -1,13 +1,15 @@
---local assassination = Traitormod.SelectedGamemode
---
---Traitormod.AddCommand("!traitoralive", function (client, args)
---    for character, traitor in pairs(assassination.Traitors) do
---        if not character.IsDead then
---            Traitormod.SendMessage(client, Traitormod.Language.TraitorsAlive)
---            return true
---        end
---    end
---
---    Traitormod.SendMessage(client, Traitormod.Language.AllTraitorsDead)
---    return true
---end)
+local assassination = Traitormod.SelectedGamemode
+
+Traitormod.AddCommand("!traitoralive", function (client, args)
+    if not client.HasPermission(ClientPermissions.ConsoleCommands) then return end
+
+    for character, traitor in pairs(assassination.Traitors) do
+        if not character.IsDead then
+            Traitormod.SendMessage(client, Traitormod.Language.TraitorsAlive)
+            return true
+        end
+    end
+
+    Traitormod.SendMessage(client, Traitormod.Language.AllTraitorsDead)
+    return true
+end)
