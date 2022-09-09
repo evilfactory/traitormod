3ae5b0b659f48c3e1c6e9de9904e410049b5596b
MassCraxx <masscraxx@users.noreply.github.com>
2022-09-09 18:15:07 +0200

    dev version

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -1,7 +1,7 @@
 if CLIENT then return end
 
 Traitormod = {}
-Traitormod.VERSION = "2.2.4"
+Traitormod.VERSION = "2.3-SNAPSHOT"
 
 print(">> Traitor Mod v" .. Traitormod.VERSION)
 print(">> Github Contributors: evilfactory, MassCraxx, Philly-V, Qunk1, mc-oofert.")
