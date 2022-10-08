0a5dc93276c16b9d862c1b2d926c762c1f3ec3bd
EvilFactory <evilfactory@users.noreply.github.com>
2022-10-08 18:39:31 -0300

    Traitor Mod 2.3

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -1,7 +1,7 @@
 if CLIENT then return end
 
 Traitormod = {}
-Traitormod.VERSION = "2.3-SNAPSHOT"
+Traitormod.VERSION = "2.3"
 
 print(">> Traitor Mod v" .. Traitormod.VERSION)
 print(">> Github Contributors: evilfactory, MassCraxx, Philly-V, Qunk1, mc-oofert.")
