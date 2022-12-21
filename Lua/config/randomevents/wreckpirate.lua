c445089e190b01e2c958be67cc498f970835a216
EvilFactory <evilfactory@users.noreply.github.com>
2022-12-21 17:30:08 -0300

    Wreck pirate is now a ghost role too and also improved ghost role feedback

--- Lua/config/randomevents/wreckpirate.lua
+++ Lua/config/randomevents/wreckpirate.lua
@@ -10,6 +10,7 @@ event.ChancePerMinute = 0.13
 event.OnlyOncePerRound = true
 
 event.AmountPoints = 1200
+event.AmountPointsPirate = 1300
 
 event.Start = function ()
     if #Level.Loaded.Wrecks == 0 then
@@ -18,7 +19,22 @@ event.Start = function ()
 
     local wreck = Level.Loaded.Wrecks[1]
 
+    local info = CharacterInfo(Identifier("human"))
+    info.Name = "Pirate " .. info.Name
+    info.Job = Job(JobPrefab.Get("mechanic"))
+
+    local character = Character.Create(info, wreck.WorldPosition, info.Name, 0, false, true)
+
+    event.Character = character
+    event.Wreck = wreck
+
+    character.CanSpeak = false
+    character.TeamID = CharacterTeamType.Team2
+    character.GiveJobItems(nil)
+
     Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.Prefabs["sonarbeacon"], wreck.WorldPosition, nil, nil, function(item)
+        item.NonInteractable = true
+
         Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.Prefabs["batterycell"], item.OwnInventory, nil, nil, function(bat)
             bat.Indestructible = true
 
@@ -28,23 +44,20 @@ event.Start = function ()
             interface.customInterfaceElementList[2].Signal = "Last known pirate position"
 
             item.CreateServerEvent(interface, interface)
-
         end)
     end)
 
-    local info = CharacterInfo(Identifier("human"))
-    info.Job = Job(JobPrefab.Get("securityofficer"))
-
-    local character = Character.Create(info, wreck.WorldPosition, info.Name, 0, false, true)
-
-    character.TeamID = CharacterTeamType.Team2
-    character.GiveJobItems(nil)
-
-    for i = 1, 4, 1 do
+    for i = 1, 2, 1 do
         Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.GetItemPrefab("shotgun"), character.Inventory, nil, nil, function (item)
             for i = 1, 6, 1 do
                 Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.GetItemPrefab("shotgunshell"), item.OwnInventory)
             end
+        end)
+    end
+
+    for i = 1, 2, 1 do
+        Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.GetItemPrefab("smg"), character.Inventory, nil, nil, function (item)
+            Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.GetItemPrefab("smgmagazine"), item.OwnInventory)
         end)
     end
 
@@ -64,6 +77,12 @@ event.Start = function ()
     local text = "There have been reports about a notorious pirate with a PUCS suit terrorizing these waters, the pirate was detected recently inside a wrecked submarine - eliminate the pirate and the crew shall claim a reward of " .. event.AmountPoints .. " points."
     Traitormod.RoundEvents.SendEventMessage(text, "CrewWalletIconLarge")
 
+    Traitormod.GhostRoles.Ask("Wreck Pirate", function (client)
+        client.SetClientCharacter(character)
+
+        Traitormod.SendMessageCharacter(character, "You are a pirate! Protect the wreck from any filthy coalitions, it will be eventually home to our center of operations! \n\nSurviving inside the wreck until the end of the round will grant you " .. event.AmountPointsPirate .." points.", "InfoFrameTabButton.Mission")
+    end)
+
     Hook.Add("think", "WreckPirate.Think", function ()
         if character.IsDead then
             event.End()
@@ -75,14 +94,24 @@ end
 event.End = function (isEndRound)
     Hook.Remove("think", "WreckPirate.Think")
 
-    if isEndRound then return end
+    if isEndRound then
+        if event.Character and not event.Character.IsDead and event.Character.Submarine == event.Wreck then
+            local client = Traitormod.FindClientCharacter(event.Character)
+            if client then
+                Traitormod.AwardPoints(client, event.AmountPointsPirate)
+                Traitormod.SendMessage(client, "You have received " .. event.AmountPointsPirate .. " points.", "InfoFrameTabButton.Mission")
+            end
+        end
+
+        return
+    end
 
     local text = "The PUCS pirate has been killed, the crew has received a reward of " .. event.AmountPoints .. " points."
 
     Traitormod.RoundEvents.SendEventMessage(text, "CrewWalletIconLarge")
 
     for _, client in pairs(Client.ClientList) do
-        if client.Character and not client.Character.IsDead then
+        if client.Character and not client.Character.IsDead and client.Character.TeamID == CharacterTeamType.Team1 then
             Traitormod.AwardPoints(client, event.AmountPoints)
             Traitormod.SendMessage(client, "You have received " .. event.AmountPoints .. " points.", "InfoFrameTabButton.Mission")
         end
--- Lua/ghostroles.lua
+++ Lua/ghostroles.lua
@@ -1,5 +1,8 @@
 local gr = {}
 
+
+local ghostRolesAnnounceTimer = 0
+
 gr.Roles = {}
 
 gr.Ask = function (name, callback)
@@ -45,7 +48,7 @@ Traitormod.AddCommand("!ghostrole", function(client, args)
 
         if roles == "" then roles = "None" end
 
-        Traitormod.SendMessage(client, "Ghost role not found, did you type the name correctly? Available roles: \n" .. roles)
+        Traitormod.SendMessage(client, "Ghost role not found, did you type the name correctly? Available roles: \n\n" .. roles)
         return true
     end
 
@@ -54,10 +57,34 @@ Traitormod.AddCommand("!ghostrole", function(client, args)
         return true
     end
 
+    Traitormod.Log(Traitormod.ClientLogName(client) .. " took the ghost role of " .. name .. ".")
+
     gr.Roles[name].Callback(client)
     gr.Roles[name].Taken = true
 end)
 
+
+Hook.Add("think", "Traitormod.GhostRoles.Think", function (...)
+    if Timer.GetTime() < ghostRolesAnnounceTimer then return end
+    ghostRolesAnnounceTimer = Timer.GetTime() + 80
+
+    local roles = ""
+    for key, value in pairs(gr.Roles) do
+        if not value.Taken then
+            roles = roles .. "\"‖color:gui.orange‖" .. key .. "\"‖color:end‖ "
+        end
+    end
+
+    if roles == "" then return end
+
+    for key, client in pairs(Client.ClientList) do
+        if client.Character == nil or client.Character.IsDead then
+            local chatMessage = ChatMessage.Create("Ghost Roles", "Ghost roles available: " .. roles .. "\n\nUse !ghostrole name to pick a role.", ChatMessageType.Default, nil, nil)
+            chatMessage.Color = Color(255, 100, 10, 255)
+            Game.SendDirectChatMessage(chatMessage, client)
+        end
+    end
+end)
 
 Hook.Add("roundEnd", "TraitorMod.GhostRoles.RoundEnd", function ()
     gr.Roles = {}
