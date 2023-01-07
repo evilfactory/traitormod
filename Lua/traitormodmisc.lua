0af373b57abb4ea687c3f35f85933c38ce540499
EvilFactory <evilfactory@users.noreply.github.com>
2023-01-06 22:41:35 -0300

    You can become watcher

--- Lua/traitormodmisc.lua
+++ Lua/traitormodmisc.lua
@@ -1,7 +1,13 @@
 local n = 1
-Hook.Add("character.created", "Traitormod.MudraptorPet", function (character)
+Hook.Add("character.created", "Traitormod.MiscGhostRoles", function (character)
     if character.SpeciesName == "Mudraptor_pet" then
         Traitormod.GhostRoles.Ask("Mudraptor Pet " .. n, function (client)
+            client.SetClientCharacter(character)
+        end, character)
+    end
+
+    if character.SpeciesName == "Watcher" then
+        Traitormod.GhostRoles.Ask("Watcher " .. n, function (client)
             client.SetClientCharacter(character)
         end, character)
     end
