098a5b22b38c591542be7c32cea365bf8911ba94
EvilFactory <evilfactory@users.noreply.github.com>
2023-05-01 19:31:21 -0300

    Added new tip

--- Lua/language/english.lua
+++ Lua/language/english.lua
@@ -12,6 +12,7 @@ language.Tips = {
     "Ghost roles might become available when you are dead, you can use !ghostrole to claim them.",
     "Typing !kill in chat as a ghost role simply returns it to the list of available ghost roles, rather than killing it.",
     "Fabricating, fixing hulls, healing and killing monsters grant you points.",
+    "Dying in the first 15 seconds as a creature refunds the price of it fully.",
 }
 
 language.Help = "\n!help - shows this help message\n!helptraitor - shows all traitor commands\n!helpadmin - lists all admin commands\n!traitor - show traitor information\n!pointshop - opens the point shop\n!points - show your points and lives\n!alive - list alive players (only while dead)\n!locatesub - shows you the distance and direction of the submarine, only for monsters\n!suicide - kills your character\n!version - shows running version of the traitormod\n!write - writes to your death logbook"
