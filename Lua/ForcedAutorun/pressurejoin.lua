919d14390a2cebba82c5e9ce63d4fa6c27acb75b
Mr. 27 <45323883+27alaing@users.noreply.github.com>
2023-05-20 09:08:12 -0400

    changed this

--- Lua/ForcedAutorun/pressurejoin.lua
+++ Lua/ForcedAutorun/pressurejoin.lua
@@ -1,13 +1,9 @@
-local extension = {}
+if CLIENT then return end
 
-extension.Init = function ()
-    Hook.Add("character.created", "Traitormod.GivePressureResistance", function(character)
-        if not character.IsHuman or character.IsBot then return end
+   Hook.Add("character.created", "Traitormod.GivePressureResistance", function(character)
+       if not character.IsHuman or character.IsBot then return end
 
-        local pressurePrefab = AfflictionPrefab.Prefabs["pressurestabilized"]
-        local limb = character.AnimController.MainLimb
-        character.CharacterHealth.ApplyAffliction(limb, pressurePrefab.Instantiate(35))
-    end)
-end
-
-return extension
+       local pressurePrefab = AfflictionPrefab.Prefabs["pressurestabilized"]
+       local limb = character.AnimController.MainLimb
+       character.CharacterHealth.ApplyAffliction(limb, pressurePrefab.Instantiate(35))
+   end)
