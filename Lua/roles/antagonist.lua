8f951d644d4caf93d9f21ceee49fc3f4592d7c3b
EvilFactory <evilfactory@users.noreply.github.com>
2023-05-26 11:24:12 -0300

    Added !tannounce for traitors

--- Lua/roles/antagonist.lua
+++ Lua/roles/antagonist.lua
@@ -40,6 +40,43 @@ Traitormod.AddCommand("!tc", function(client, args)
     return true
 end)
 
+Traitormod.AddCommand("!tannounce", function(client, args)
+    local feedback = Traitormod.Language.CommandNotActive
+
+    local clientRole = Traitormod.RoleManager.GetRole(client.Character)
+
+    if clientRole == nil or client.Character.IsDead then
+        feedback = Traitormod.Language.NoTraitor
+    elseif not clientRole.TraitorBroadcast then
+        feedback = Traitormod.Language.CommandNotActive
+    elseif #args > 0 then
+        local msg = ""
+        for word in args do
+            msg = msg .. " " .. word
+        end
+
+        for character, role in pairs(Traitormod.RoleManager.RoundRoles) do
+            if role.TraitorBroadcast then
+                local targetClient = Traitormod.FindClientCharacter(character)
+
+                if targetClient then
+                    Game.SendDirectChatMessage("",
+                        string.format(Traitormod.Language.TraitorBroadcast, client, msg), nil,
+                        ChatMessageType.ServerMessageBoxInGame, targetClient)
+                end
+            end
+        end
+
+        return not clientRole.TraitorBroadcastHearable
+    else
+        feedback = "Usage: !tannounce [Message]"
+    end
+
+    Game.SendDirectChatMessage("", feedback, nil, Traitormod.Config.ChatMessageType, client)
+
+    return true
+end)
+
 Traitormod.AddCommand("!tdm", function(client, args)
     local feedback = ""
 
