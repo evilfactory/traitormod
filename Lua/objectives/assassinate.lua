6b18402b6185a079f82d36e6b03d702790a0c04b
27alaing <koolthunder019@gmail.com>
2023-06-01 15:18:53 -0400

    role filter to assassinate

--- Lua/objectives/assassinate.lua
+++ Lua/objectives/assassinate.lua
@@ -2,6 +2,15 @@ local objective = Traitormod.RoleManager.Objectives.Objective:new()
 
 objective.Name = "Assassinate"
 objective.AmountPoints = 600
+objective.RoleFilter = { 
+    ["he-chef"] = true,
+    ["staff"] = true,
+    ["janitor"] = true,
+    ["prisondoctor"] = true,
+    ["guard"] = true,
+    ["warden"] = true,
+    ["headguard"] = true,
+}
 
 function objective:Start(target)
     self.Target = target
