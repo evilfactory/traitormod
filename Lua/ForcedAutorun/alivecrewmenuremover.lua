391a31efe3289f66dbd0149ac0e2f21f952a9ac4
Mr. 27 <45323883+27alaing@users.noreply.github.com>
2023-06-01 17:41:40 -0400

    Update alivecrewmenuremover.lua

--- Lua/ForcedAutorun/alivecrewmenuremover.lua
+++ Lua/ForcedAutorun/alivecrewmenuremover.lua
@@ -1,7 +1,7 @@
 if CLIENT then return end
 
 Hook.Add("character.created", "crewMenuRemoverOnCharacterCreation", function (character)
-    if character.IsHuman and not character.IsBot then
+    if character and character.IsHuman and not character.IsBot then
         Networking.CreateEntityEvent(character, Character.RemoveFromCrewEventData.__new(value.Character.TeamID, {}))
     end
 end)
