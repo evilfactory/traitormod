808b007635fff1e225d5676d894b85afeef4fe34
27alaing <koolthunder019@gmail.com>
2023-06-01 14:51:08 -0400

    Didn't realize this
    There's already an !alive command in traitormod. Made it say the JobIdentifier on that one

--- Lua/ForcedAutorun/alivecrewmenuremover.lua
+++ Lua/ForcedAutorun/alivecrewmenuremover.lua
@@ -5,29 +5,3 @@ Hook.Add("roundStart", "crewmenuRoundStart", function ()
         Networking.CreateEntityEvent(value, Character.RemoveFromCrewEventData.__new(value.TeamID, {}))
     end
 end)
-
-Hook.Add("chatMessage", "crewmenu_chatcommands", function(msg, client)
-
-    if msg == "!alive" then
-        if client.Character == nil or client.Character.IsDead == true or bit32.band(client.Permissions, 0x40) == 0x40 then
-
-            local msg = ""
-            for key, value in pairs(Character.CharacterList) do
-
-                if value.IsHuman and not value.IsBot then
-                    print(value.IsDead)
-                    if value.IsDead then
-                        msg = msg .. "[DEAD] " ..value.name.. " as a "..value.JobIdentifier.. "\n"
-                    else
-                        msg = msg .. "[ALIVE] " ..value.name.. " as a "..value.JobIdentifier.."\n"
-                    end
-                end
-            end
-
-            Game.SendDirectChatMessage("", msg, nil, 7, client)
-
-            return true
-        end
-
-    end
-end)
