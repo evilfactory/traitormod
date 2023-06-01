c653379aa7ad15c50396dd71fb38f1fac4e65b2f
27alaing <koolthunder019@gmail.com>
2023-06-01 17:13:36 -0400

    changed from roundstart to character.created
    for midround joiners

--- Lua/ForcedAutorun/alivecrewmenuremover.lua
+++ Lua/ForcedAutorun/alivecrewmenuremover.lua
@@ -1,7 +1,7 @@
 if CLIENT then return end
 
-Hook.Add("roundStart", "crewmenuRoundStart", function ()
-    for key, value in pairs(Client.ClientList) do
-        Networking.CreateEntityEvent(value.Character, Character.RemoveFromCrewEventData.__new(value.Character.TeamID, {}))
+Hook.Add("character.created", "crewMenuRemoverOnCharacterCreation", function (character)
+    if character.IsHuman and not character.IsBot then
+        Networking.CreateEntityEvent(character, Character.RemoveFromCrewEventData.__new(value.Character.TeamID, {}))
     end
 end)
