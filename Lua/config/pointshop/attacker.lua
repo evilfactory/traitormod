b0d76b9527e0b3f4785c97ed79aee64ba0f9cde5
TheDarkBatman <87569239+TheDarkBatman2@users.noreply.github.com>
2023-06-04 14:42:01 +0330

    attacker/defender pointsshop

--- Lua/config/pointshop/attacker.lua
+++ Lua/config/pointshop/attacker.lua
@@ -3,6 +3,10 @@ local category = {}
 category.Identifier = "attacker"
 category.Gamemode = "AttackDefend"
 
+category.CanAccess = function(client)
+    return client.Character and not client.Character.IsDead and Traitormod.RoleManager.HasRole(client.Character, "Attacker")
+end
+
 category.Products = {
     {
         Price = 90,
