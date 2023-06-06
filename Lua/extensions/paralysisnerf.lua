663a3cd91b082eef1c17e3dbe11608c0d6fd1227
EvilFactory <evilfactory@users.noreply.github.com>
2023-06-06 12:39:47 -0300

    Fixed paralysis not decreasing slowparalysis

--- Lua/extensions/paralysisnerf.lua
+++ Lua/extensions/paralysisnerf.lua
@@ -7,11 +7,24 @@ extension.CureTime = 60 * 60 * 7 -- 7 minutes
 extension.Init = function ()
     local timer = {}
 
+    local function GetParalysisAmount(character)
+        local paralysis = character.CharacterHealth.GetAfflictionStrength("paralysis")
+        if paralysis > 0 then
+            return paralysis
+        end
+
+        local slowParalysis = character.CharacterHealth.GetAfflictionStrength("slowparalysis")
+        if slowParalysis > 0 then
+            return slowParalysis
+        end
+        return 0
+    end
+
     Hook.Add("think", "ParalysisNerf", function (...)
         for _, client in pairs(Client.ClientList) do
             local character = client.Character
             if character then
-                if character.CharacterHealth.GetAfflictionStrength("paralysis") > 0 then
+                if GetParalysisAmount(character) > 0 then
                     if not timer[character] then
                         timer[character] = 0
                     end
@@ -28,6 +41,9 @@ extension.Init = function ()
                 local affResistance = character.CharacterHealth.GetAffliction("paralysisresistance")
                 if affResistance and affResistance.Strength > 0 then
                     local affliction = character.CharacterHealth.GetAffliction("paralysis", true)
+                    if affliction == nil then
+                        affliction = character.CharacterHealth.GetAffliction("slowparalysis", true)
+                    end
                     if affliction then
                         affliction._strength = affliction._strength - 0.05
                     end
@@ -40,5 +56,6 @@ extension.Init = function ()
         timer = {}
     end)
 end
+
 
 return extension
