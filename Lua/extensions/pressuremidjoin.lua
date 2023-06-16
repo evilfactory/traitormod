14d10f7e531db17a87df7e722907ef8dfd9cf788
27alaing <koolthunder019@gmail.com>
2023-06-16 02:23:41 -0400

    spirit games

--- Lua/extensions/pressuremidjoin.lua
+++ Lua/extensions/pressuremidjoin.lua
@@ -4,12 +4,11 @@ extension.Identifier = "pressuremidjoin"
 
 extension.Init = function ()
     Hook.Add("character.created", "Traitormod.GivePressureResistance", function(character)
+        if character.IsBot or not character.IsHuman then return end
         local pressurePrefab = AfflictionPrefab.Prefabs["pressurestabilized"].Instantiate(30)
         local limb = character.AnimController.MainLimb
         character.CharacterHealth.ApplyAffliction(limb, pressurePrefab)
-        if character.TeamID == CharacterTeamType.Team1 then
-            Networking.CreateEntityEvent(character, Character.RemoveFromCrewEventData.__new(character.TeamID, {}))
-        end
+        Networking.CreateEntityEvent(character, Character.RemoveFromCrewEventData.__new(character.TeamID, {}))
     end)
 end
 
