a9882a7ab4bcf95b6908210bb7df28b0a0ac103d
Mr. 27 <45323883+27alaing@users.noreply.github.com>
2023-06-21 11:42:58 -0400

    make chemicals objective

--- Lua/objectives/crew/makedrugs.lua
+++ Lua/objectives/crew/makedrugs.lua
@@ -5,15 +5,29 @@ objective.AmountPoints = 550
 objective.Progress = 0
 
 function objective:Start(target)
-    self.Text = "Make ("..objective.Progress.."/5) medical items."
+    self.Text = "Make ("..objective.Progress.."/5) chemicals or opioids."
 
     Hook.Add("item.created", "MedicObjective", function (item)
-        Timer.Wait(function ()
-            if item.HasTag("medical") and not item.HasTag("notarget") then
-                objective.Progress = objective.Progress + 1
-                self.Text = "Make ("..objective.Progress.."/5) medical items."
-            end
-        end, 6000)
+        local parent = item.ParentInventory
+            
+        if parent == nil then return end
+        if LuaUserData.IsTargetType(parent.Owner, "Barotrauma.Character") then return end
+        if parent.Owner.Prefab.Identifier ~= "medicalfabricator" then return end
+            
+        if (item.Identifier == "antidama1"
+             or item.Identifier == "antidama2"
+             or item.Identifier == "combatstimulantsyringe"
+             or item.Identifier == "opium"
+             or item.Identifier == "steroids"
+             or item.Identifier == "deusizine"
+             or item.Identifier == "meth"
+             or item.Identifier == "pomegrenadeextract"
+             or item.Identifier == "tonicliquid"
+             or item.Identifier == "pressurestabilizer")
+         then
+            objective.Progress = objective.Progress + 1
+            self.Text = "Make ("..objective.Progress.."/5) chemicals or opioids."
+         end
     end)
 
     return true
