965354ca0db4416b06bf75ae33bc1cc2ee4bbfb6
27alaing <koolthunder019@gmail.com>
2023-06-25 15:36:51 -0400

    randomize name on spawn

--- Lua/extensions/pressuremidjoin.lua
+++ Lua/extensions/pressuremidjoin.lua
@@ -9,6 +9,9 @@ extension.Init = function ()
         local limb = character.AnimController.MainLimb
         character.CharacterHealth.ApplyAffliction(limb, pressurePrefab)
         Networking.CreateEntityEvent(character, Character.RemoveFromCrewEventData.__new(character.TeamID, {}))
+
+        local info = CharacterInfo(Identifier("human"))
+        Character.Info.Name = info.Name
     end)
 end
 
