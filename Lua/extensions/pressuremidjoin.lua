8ff8b4590554177b096b782fdaa0295f05adfe2c
Mr. 27 <45323883+27alaing@users.noreply.github.com>
2023-06-25 14:00:38 -0400

    Update pressuremidjoin.lua

--- Lua/extensions/pressuremidjoin.lua
+++ Lua/extensions/pressuremidjoin.lua
@@ -4,7 +4,7 @@ extension.Identifier = "pressuremidjoin"
 
 extension.Init = function ()
     Hook.Add("character.created", "Traitormod.GivePressureResistance", function(character)
-        if character.IsBot or not character.IsHuman then return end
+        if not character.IsHuman then return end
         local pressurePrefab = AfflictionPrefab.Prefabs["pressurestabilized"].Instantiate(30)
         local limb = character.AnimController.MainLimb
         character.CharacterHealth.ApplyAffliction(limb, pressurePrefab)
