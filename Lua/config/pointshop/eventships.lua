940edf784783bf861d0eb8189ea5ca9775be1ca9
27alaing <45323883+27alaing@users.noreply.github.com>
2023-07-02 14:51:26 -0400

    added 4 new event ships

--- Lua/config/pointshop/eventships.lua
+++ Lua/config/pointshop/eventships.lua
@@ -12,11 +12,12 @@ end
 category.Init = function ()
     if Traitormod.SubmarineBuilder then
         category.StreamChalkId = Traitormod.SubmarineBuilder.AddSubmarine(Traitormod.Path .. "/Submarines/Stream Chalk.sub", "[P]Stream Chalk")
-        category.BarsukId = Traitormod.SubmarineBuilder.AddSubmarine("Content/Submarines/Barsuk.sub", "[P]Barsuk")
-        category.SeaShark = Traitormod.SubmarineBuilder.AddSubmarine(Traitormod.Path .. "/Submarines/Sea shark Mark II.sub", "[P]Sea shark Mark II")
-        category.Uri = Traitormod.SubmarineBuilder.AddSubmarine(Traitormod.Path .. "/Submarines/Uri.sub", "[P]Uri - Alien Ship")
-        category.CoalitionRescueShuttle = Traitormod.SubmarineBuilder.AddSubmarine(Traitormod.Path .. "/Submarines/Coalition Hemulen.sub", "Rescue Shuttle")
+        category.Separatist = Traitormod.SubmarineBuilder.AddSubmarine("Content/Submarines/Separtist_Fighter.sub", "Separatist Fighter Ship")
+        category.Unknown = Traitormod.SubmarineBuilder.AddSubmarine(Traitormod.Path .. "/Submarines/Unknown.sub", "Unknown")
+        category.Coalition_Dropship = Traitormod.SubmarineBuilder.AddSubmarine(Traitormod.Path .. "/Submarines/Coalition_Dropship.sub", "Coalition Dropship")
+        category.CoalitionRescueShuttle = Traitormod.SubmarineBuilder.AddSubmarine(Traitormod.Path .. "/Submarines/Coalition Hemulen.sub", "Coalition Rescue Shuttle")
         category.PrisonerTransport = Traitormod.SubmarineBuilder.AddSubmarine(Traitormod.Path .. "/Submarines/Prisoner Transport Barsuk.sub", "Prisoner Transport Barsuk")
+        category.Unknown_Signal = Traitormod.SubmarineBuilder.AddSubmarine(Traitormod.Path .. "/Submarines/Unknown_Signal.sub", "Mysterious Signal")
     end
 end
 
@@ -73,32 +74,32 @@ category.Products = {
     },
 
     {
-        Identifier = "uri",
+        Identifier = "Dropship",
         Price = 0,
         Limit = 1,
         IsLimitGlobal = true,
 
         Action = function (client, product, items)
-            SpawnSubmarine(category.Uri, client)
+            SpawnSubmarine(category.Coalition_Dropship, client)
         end,
 
         CanBuy = function (client, product)
-            return CanBuy(category.Uri, client)
+            return CanBuy(category.Coalition_Dropship, client)
         end
     },
 
     {
-        Identifier = "seashark",
+        Identifier = "separatist",
         Price = 0,
         Limit = 1,
         IsLimitGlobal = true,
 
         Action = function (client, product, items)
-            SpawnSubmarine(category.SeaShark, client)
+            SpawnSubmarine(category.Separatist, client)
         end,
 
         CanBuy = function (client, product)
-            return CanBuy(category.SeaShark, client)
+            return CanBuy(category.Separatist, client)
         end
     },
 
@@ -135,18 +136,34 @@ category.Products = {
     },
 
     {
-        Identifier = "barsuk",
+        Identifier = "unknownwreck",
         Price = 0,
         Limit = 1,
         IsLimitGlobal = true,
 
         Action = function (client, product, items)
-            local submarine = SpawnSubmarine(category.BarsukId, client)
+            local submarine = SpawnSubmarine(category.Unknown, client)
             AutoItemPlacer.RegenerateLoot(submarine, nil)
         end,
 
         CanBuy = function (client, product)
-            return CanBuy(category.BarsukId, client)
+            return CanBuy(category.Unknown, client)
+        end
+    },
+
+    {
+        Identifier = "unknownsignal",
+        Price = 0,
+        Limit = 1,
+        IsLimitGlobal = true,
+
+        Action = function (client, product, items)
+            local submarine = SpawnSubmarine(category.Unknown_Signal, client)
+            AutoItemPlacer.RegenerateLoot(submarine, nil)
+        end,
+
+        CanBuy = function (client, product)
+            return CanBuy(category.Unknown_Signal, client)
         end
     },
 }
