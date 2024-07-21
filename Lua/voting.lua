6a7bbcbf4e5f5ef1a7c7935932d409f4d4d0a606
Austinizer <148470346+Dr-javier@users.noreply.github.com>
2024-07-21 17:15:20 -0500

    newtest1

--- Lua/voting.lua
+++ Lua/voting.lua
@@ -92,52 +92,108 @@ Traitormod.AddCommand("!vote", function (client, args)
     return true
 end)
 
-Traitormod.AddCommand({"!votewarden", "!wardenvote"}, function (client, args)
+Traitormod.AddCommand({"!startwardenvote"}, function (client, args)
     if not client.HasPermission(ClientPermissions.ConsoleCommands) then return end
-    if client.InGame then
-        Traitormod.SendMessage(client, "You must be in game lobby to use this command.")
+    if not client.InGame then
+        Traitormod.SendMessage(client, "You must be in game to use this command.")
         return true
     end
 
     -- Filter clients with the "warden" job
-    local wardenClients = {}
+    wardenCandidates = {}
     for _, client in pairs(Client.ClientList) do
-        if client.PreferredJob == "warden" then
-            table.insert(wardenClients, client)
+        if client.AssignedJob and client.AssignedJob.Prefab.Identifier == "warden" then
+            table.insert(wardenCandidates, client)
         end
     end
 
-    if #wardenClients == 0 then
+    if #wardenCandidates == 0 then
         Traitormod.SendMessage(client, "No clients have the 'warden' job.")
         return true
     end
 
-    local options = {}
-    for _, wardenClient in ipairs(wardenClients) do
-        table.insert(options, wardenClient.Name)
+    wardenVotes = {}
+    wardenVoteInProgress = true
+    wardenVoteTimer = Timer.GetTime() + 25 -- 25 seconds vote time
+
+    -- Announce vote start and options
+    Traitormod.SendMessage(nil, "Vote for Warden started!")
+    Traitormod.SendMessage(nil, "Options:")
+    for i, candidate in ipairs(wardenCandidates) do
+        Traitormod.SendMessage(nil, string.format("%d. %s", i, candidate.Name))
     end
 
-    vt.StartVote("Vote for Warden", options, 25, function (results)
-        local message = Traitormod.StringBuilder:new()
-        message("Warden Vote Results:\n\n")
-        for key, value in pairs(results) do
-            message("%s: %s Votes\n", options[key], value)
-        end
+    return true
+end)
 
-        for _, target in pairs(Client.ClientList) do
-            local chatMessage = ChatMessage.Create("", message:concat(), ChatMessageType.Default, nil, nil)
-            chatMessage.Color = Color(255, 255, 255, 255)
-            Game.SendDirectChatMessage(chatMessage, target)
-        end
+Traitormod.AddCommand({"!votewarden"}, function (client, args)
+    if not wardenVoteInProgress then
+        Traitormod.SendMessage(client, "No warden vote in progress.")
+        return true
+    end
 
-        -- Store the results
-        wardenVoteResults = {}
-        for key, value in pairs(results) do
-            wardenVoteResults[options[key]] = value
-        end
-    end)
+    if #args < 1 then
+        Traitormod.SendMessage(client, "Usage: !votewarden (number)")
+        return true
+    end
+
+    local voteNumber = tonumber(args[1])
+    if not voteNumber or voteNumber < 1 or voteNumber > #wardenCandidates then
+        Traitormod.SendMessage(client, "Invalid vote number.")
+        return true
+    end
+
+    wardenVotes[client] = voteNumber
+    Traitormod.SendMessage(client, "Your vote has been cast.")
 
     return true
+end)
+
+Hook.Add("think", "Traitormod.WardenVoting.Think", function ()
+    if wardenVoteInProgress and Timer.GetTime() > wardenVoteTimer then
+        wardenVoteInProgress = false
+
+        -- Count votes
+        local voteCounts = {}
+        for i = 1, #wardenCandidates do
+            voteCounts[i] = 0
+        end
+
+        for _, vote in pairs(wardenVotes) do
+            if voteCounts[vote] then
+                voteCounts[vote] = voteCounts[vote] + 1
+            end
+        end
+
+        -- Determine the winner
+        local maxVotes = 0
+        local winnerIndex = nil
+        for i, count in ipairs(voteCounts) do
+            if count > maxVotes then
+                maxVotes = count
+                winnerIndex = i
+            end
+        end
+
+        if winnerIndex then
+            local winner = wardenCandidates[winnerIndex]
+            Traitormod.SendMessage(nil, string.format("Warden Vote Results:\nWinner: %s with %d votes", winner.Name, maxVotes))
+        else
+            Traitormod.SendMessage(nil, "Warden Vote Results:\nNo votes cast.")
+        end
+
+        -- Assign warden role based on vote results
+        for _, client in pairs(Client.ClientList) do
+            local jobName = client.AssignedJob.Prefab.Identifier.ToString()
+            if jobName == "warden" and client ~= wardenCandidates[winnerIndex] then
+                local validJobs = { "prisondoctor", "guard", "headguard", "staff", "janitor", "convict", "he-chef" }
+                local newJobName = validJobs[math.random(1, #validJobs)]
+                client.AssignedJob = Traitormod.GetJobVariant(newJobName)
+                Traitormod.SendMessage(client, "You have been reassigned from the warden role to: " .. newJobName)
+                print(string.format("Client %s reassigned to new job %s due to warden vote", client.Name, newJobName))
+            end
+        end
+    end
 end)
 
 return vt
