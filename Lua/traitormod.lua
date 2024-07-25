36bfee304eb8896f07a487be988bf6a16686d971
Austinizer <148470346+Dr-javier@users.noreply.github.com>
2024-07-24 22:33:57 -0500

    forgot to run it

--- Lua/traitormod.lua
+++ Lua/traitormod.lua
@@ -454,6 +454,7 @@ dofile(Traitormod.Path .. "/Lua/statistics.lua")
 dofile(Traitormod.Path .. "/Lua/respawnshuttle.lua")
 dofile(Traitormod.Path .. "/Lua/traitormodmisc.lua")
 dofile(Traitormod.Path .. "/Lua/logstuff.lua")
+dofile(Traitormod.Path .. "/Lua/searchfunction.lua")
 
 Traitormod.AddGamemode(dofile(Traitormod.Path .. "/Lua/gamemodes/gamemode.lua"))
 Traitormod.AddGamemode(dofile(Traitormod.Path .. "/Lua/gamemodes/secret.lua"))
