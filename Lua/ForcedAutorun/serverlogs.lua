dadc742ee74e2857758f72b20091a1d3f77d6201
Mr. 27 <45323883+Dutch-VanDerLinde@users.noreply.github.com>
2024-08-28 00:38:26 -0400

    Update serverlogs.lua

--- Lua/ForcedAutorun/serverlogs.lua
+++ Lua/ForcedAutorun/serverlogs.lua
@@ -10,14 +10,14 @@ local maxSingleMessages = 15
 
 local messageTimer = 0
 
-Hook.Add("serverLog", "neurorp_discordIntegrationForLogs", function (logMessage, logType)
+Hook.Add("serverLog", "prisonRP_discordIntegrationForLogs", function (logMessage, logType)
     local escapedName = escapeQuotes(tostring(logType))
     local escapedMessage = escapeQuotes(tostring(logMessage))
 
     messageBuffer[escapedMessage] = escapedName
 end)
 
-Hook.Add("think", "neurorp_sendBufferedMessages", function ()
+Hook.Add("think", "prisonRP_sendBufferedMessages", function ()
     if Timer.GetTime() < messageTimer then return end
 
     local appendedMessage = ""
