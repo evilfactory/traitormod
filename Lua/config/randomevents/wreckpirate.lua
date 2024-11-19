81dd7cdf6f2918c02a4a2f8afab2351e0ba807c8
Huntonius4 <132175840+Huntonius4@users.noreply.github.com>
2024-11-18 21:03:21 -0700

    Removed some LO2 tanks wreckpirate.lua
    Removed two of the extra liquid oxygenite tanks that spawn with the wreck pirate to reduce inventory clutter as they no longer stack

--- Lua/config/randomevents/wreckpirate.lua
+++ Lua/config/randomevents/wreckpirate.lua
@@ -78,8 +78,6 @@ event.Start = function ()
     Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.GetItemPrefab("antiparalysis"), character.Inventory)
     Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.GetItemPrefab("oxygenitetank"), character.Inventory)
     Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.GetItemPrefab("oxygenitetank"), character.Inventory)
-    Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.GetItemPrefab("oxygenitetank"), character.Inventory)
-    Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.GetItemPrefab("oxygenitetank"), character.Inventory)
 
     for i = 1, 12, 1 do
         Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.GetItemPrefab("shotgunshell"), character.Inventory)
