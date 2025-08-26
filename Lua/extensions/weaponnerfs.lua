1a8f4f4984d7aa6a0c6801157b0d9eb044847794
Huntonius4 <132175840+Huntonius4@users.noreply.github.com>
2025-08-26 10:30:09 -0600

    Nerfed dementonite crowbar (weaponnerfs.lua)

--- Lua/extensions/weaponnerfs.lua
+++ Lua/extensions/weaponnerfs.lua
@@ -3,7 +3,7 @@ local extension = {}
 extension.Identifier = "weaponnerfs"
 
 extension.Init = function ()
-    do -- Ceremonial Sword
+    do -- Ceremonial Sword. Vanilla ones shred anyone. These ones instead cause massive bleeding.
         local replacement = [[
         <overwrite>
             <Attack targetimpulse="2" severlimbsprobability="0.55" itemdamage="10" structuredamage="1" structuresoundtype="StructureSlash">
@@ -20,11 +20,11 @@ extension.Init = function ()
         Traitormod.Patching.Add(element, replacement)
     end
 
-    do -- Hardened Crowbar
+    do -- Hardened Crowbar. No longer capable of killing everyone. Gives more radiation sickness.
         local replacement = [[
         <overwrite>
             <Attack targetimpulse="13" penetration="0.25">
-                <Affliction identifier="blunttrauma" strength="14" />
+                <Affliction identifier="blunttrauma" strength="15" />
                 <Affliction identifier="radiationsickness" strength="5" />
                 <Affliction identifier="stun" strength="0.1" />
             </Attack>
@@ -32,6 +32,22 @@ extension.Init = function ()
         ]]
 
         local itemPrefab = ItemPrefab.GetItemPrefab("crowbarhardened")
+        local element = itemPrefab.ConfigElement.Element.Element("MeleeWeapon")
+        Traitormod.Patching.RemoveAll(element, "Attack")
+        Traitormod.Patching.Add(element, replacement)
+    end
+
+    do -- Dementonite Crowbar. Slightly stronger than hardened crowbar since it can't be crafted. Also gives brain damage.
+        local replacement = [[
+        <overwrite>
+            <Attack targetimpulse="13" penetration="0.25">
+                <Affliction identifier="blunttrauma" strength="16" /> 
+                <Affliction identifier="psychosis" strength="25" />
+                <Affliction identifier="stun" strength="0.1" />
+            </Attack>
+        </overwrite>
+        ]]
+        local itemPrefab = ItemPrefab.GetItemPrefab("crowbardementonite")
         local element = itemPrefab.ConfigElement.Element.Element("MeleeWeapon")
         Traitormod.Patching.RemoveAll(element, "Attack")
         Traitormod.Patching.Add(element, replacement)
